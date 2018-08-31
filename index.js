a546a0232d295bdf9781454679fe1bc341dd3d41
Tara E. Sabella <tarasabella@gmail.com>
2018-08-31 17:29:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase()); 
 }
 function sayHiToGrandma(string) {
-  if (string).toLowerCase();
+  if (string.toLowerCase();
   return "I can\'t hear you!"; 
   } 
